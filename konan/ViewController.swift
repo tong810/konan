d776ad41c1b4677a9db66ef17ed8d7820f816f83
tong <zz472780@gmail.com>
2024-07-24 14:47:58 +0800

    Initial Commit

---
+++ konan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  konan
+//
+//  Created by 陳曉潼 on 2024/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
